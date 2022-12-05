e248263adddfa87a878c86f08ff54998f72aea9e
Dan2625 <110021649+Dan2625@users.noreply.github.com>
2022-12-05 14:59:17 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const dbConnect = require('./db/dbConnect');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const auth = require('./auth');
+const User = require('./db/userModal');
+
+dbConnect();
+
+// Curb Cores Error by adding a header here
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+  );
+  next();
+});
+
+const { response } = require('express');
+// body parser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (request, response, next) => {
+  response.json({ message: 'Hey! This is your server response!' });
+  next();
+});
+
+app.post('/register', (request, response) => {
+  bcrypt
+    .hash(request.body.password, 10)
+    .then((hashedPassword) => {
+      const user = new User({
+        username: request.body.username,
+        password: hashedPassword,
+      });
+      user
+        .save()
+        .then((result) => {
+          response.status(201).send({
+            message: 'User Created Successfully',
+            result,
+          });
+        })
+        .catch((error) => {
+          response.status(500).send({
+            message: 'Error creating user',
+            error,
+          });
+        });
+    })
+    .catch((e) => {
+      response.status(500).send({
+        message: 'failed to hased the password',
+        e,
+      });
+    });
+});
+
+// login endpoint
+app.post('/login', (request, response) => {
+  // check if username exists
+  User.findOne({ username: request.body.username })
+    .then((user) => {
+      bcrypt
+        .compare(request.body.password, user.password)
+
+        .then((passwordCheck) => {
+          if (!passwordCheck) {
+            return response.status(400).send({
+              message: 'Passwords does not match',
+              error,
+            });
+          }
+
+          //   create JWT token
+          const token = jwt.sign(
+            {
+              userId: user._id,
+              userUsername: user.username,
+            },
+            'RANDOM-TOKEN',
+            { expiresIn: '24h' }
+          );
+
+          //   return success response
+          response.status(200).send({
+            message: 'Login Successful',
+            username: user.username,
+            token,
+          });
+        })
+        // catch error if password does not match
+        .catch((error) => {
+          response.status(400).send({
+            message: 'Passwords does not match',
+            error,
+          });
+        });
+    })
+    // catch error if username does not exist
+    .catch((e) => {
+      response.status(404).send({
+        message: 'Username not found',
+        e,
+      });
+    });
+});
+
+// free endpoint
+app.get('/free-endpoint', (request, response) => {
+  response.json({ message: 'You are free to access me anytime' });
+});
+
+// authentication endpoint
+app.get('/auth-endpoint', auth, (request, response) => {
+  response.json({ message: 'You are authorized to access me' });
+});
+
+module.exports = app;
